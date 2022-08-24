b3e929846742cd627dda3f839149b580d80a1d5f
LuisEmilioPizzolanti <luisemilio1990@hotmail.com>
2022-08-24 11:57:23 -0300

    quitado un if inecesario

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ let int_valor1
 let int_valor2
 do {
     str_Opcion = prompt("Seleccione una operacion: \n 1 - Suma \n 2 - Resta \n 3 - Multiplicacion \n 4 - Divicion \n 5 - Salir")
-    if (str_Opcion === "5") {
-        alert("Gracias por usar calculadora de coderJavascript Luis Emilio Pizzolanti")
-        break;
-    }
 
     switch (str_Opcion) {
         case "1":
@@ -27,9 +23,9 @@ do {
             alert("El resultado de la divicion " + int_valor1 + " / " + int_valor2 + " = " + (int_valor1 / int_valor2))
             break
         case "5":
-            alert("Gracias por usar calculadora javascript")
-            break;
+            alert("Gracias por usar calculadora de coderJavascript Luis Emilio Pizzolanti")
             bool_Continuar = false
+            break;
         default:
             alert("La opcion ingresada no es correcta, seleccione una opcion valida.")
 
