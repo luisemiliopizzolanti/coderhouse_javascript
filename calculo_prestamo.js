5bafaf72deaf53c425398f744a93a6f5b89f40f1
LuisEmilioPizzolanti <luisemilio1990@hotmail.com>
2022-09-26 00:26:12 -0300

    agregado cambios de eventos, json

--- calculo_prestamo.js
+++ calculo_prestamo.js
@@ -7,19 +7,96 @@ let bool_opcionCorrecta
 let arr_prestamosSimulados = []
 let str_todosLosPrestamos = "Prestamos simulados: \n"
 
-alert("Bienvenido al simulador de Prestamos")
+//alert("Bienvenido al simulador de Prestamos")
 
-str_opcion = prompt("¿Desea simular un prestamo? \n 1 - Si \n 2 - No")
+let formPrestamo = document.getElementById("prestamo")
+
+
+formPrestamo.onsubmit = (event) => {
+    event.preventDefault()
+    const arr_datosPrestamo = []
+    for (const input of event.target.children) {
+        //console.log(input)
+        if (input.id === "cuotas") {
+            arr_datosPrestamo['cuotas'] = parseInt(input.value)
+        }
+        if (input.id === "monto") {
+            arr_datosPrestamo['monto'] = parseInt(input.value)
+        }
+    }
+
+    if (arr_datosPrestamo['monto'] <= 0) {
+        alert("No ingreso un monto valido, intente nuevamente")
+        bool_opcionCorrecta = false
+    } else {
+        bool_opcionCorrecta = true
+    }
+    if (arr_datosPrestamo['cuotas'] < 1 || arr_datosPrestamo['cuotas'] > 12) {
+        alert("No ingreso un periodo valido, intente nuevamente")
+        bool_opcionCorrecta = false
+    } else {
+        bool_opcionCorrecta = true
+    }
+
+    int_valorCuota = calcularPrestamo(arr_datosPrestamo['monto'], int_intereses, arr_datosPrestamo['cuotas']) / arr_datosPrestamo['cuotas']
+    arr_datosPrestamo['valorCuota'] = int_valorCuota
+    arr_prestamosSimulados.push(arr_datosPrestamo)
+        // console.log(arr_prestamosSimulados)
+    localStorage.setItem("prestamosSolicitados", JSON.stringify(arr_prestamosSimulados))
+        //Muestra arrays vacios
+    console.log(JSON.stringify(arr_prestamosSimulados))
+        //mostrarPrestamos()
+        //alert(int_valorCuota)
+        // const historico = document.createElement('p')
+        //historico.innerText = int_valorCuota
+
+
+    //divPrestamos.append(historico)
+}
+
+function mostrarPrestamos() {
+    let divPrestamos = document.getElementById('todosLosPrestamos')
+    divPrestamos.innerHTML = ""
+    let c, r, t
+    let arr_prestamos
+    t = document.createElement('table');
+    json_prestamos = localStorage.getItem('prestamosSolicitados')
+
+    arr_prestamos = JSON.parse(json_prestamos)
+
+    arr_prestamos.forEach(prestamo => {
+        r = t.insertRow(-1);
+        c = r.insertCell();
+        console.log(prestamo)
+        c.innerHTML = prestamo["monto"];
+        c = r.insertCell();
+        c.innerHTML = prestamo["cuotas"];
+        c = r.insertCell();
+        c.innerHTML = prestamo["valorCuota"];
+
+
+
+    });
+
+
+
+    /* c.innerHTML = 123;
+     c = r.insertCell(1);
+     c.innerHTML = 456;*/
+    divPrestamos.append(t)
+        //divPrestamos.removeChild(divPrestamos.firstChild)
+}
+
+/*
+        int_montoASolicitar = parseInt(prompt("Ingrese monto a solicitar"))
+        
+}
+
+
 if (str_opcion === "1") {
     do {
         do {
-            int_montoASolicitar = parseInt(prompt("Ingrese monto a solicitar"))
-            if (int_montoASolicitar <= 0) {
-                alert("No ingreso un monto valido, intente nuevamente")
-                bool_opcionCorrecta = false
-            } else {
-                bool_opcionCorrecta = true
-            }
+
         }
         while (!bool_opcionCorrecta)
         do {
@@ -45,8 +122,7 @@ if (str_opcion === "1") {
     divPrestamos.append(historico)
 
 };
-
-alert("Gracias por usar el simulador de prestamos CoderHouse - Luis Emilio Pizzolanti")
+*/
 
 function calcularPrestamo(pInt_montoSolicitado, pInt_tasaInteres, pInt_peridodoMeses) {
     //La funcion utilizada para caluclar el capital a devolver es la de interes compuesto
